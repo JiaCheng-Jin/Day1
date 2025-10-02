c35920fd793ad7c34cae133fdd060370f49c4808
JiaCheng-Jin <jjc136611@sjtu.edu.cn>
2025-10-02 15:07:46 +0800

    fix: Change PWM Pattern

--- Core/Src/callback.cpp
+++ Core/Src/callback.cpp
@@ -18,7 +18,9 @@ void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin)
 
 // Override __weak
 void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim) {
-  uint32_t arr_value = __HAL_TIM_GET_AUTORELOAD(htim) + 1;
-  uint32_t brightness = arr_value * sinf(4 * HAL_GetTick() / 1000.0f) - 1;
-  __HAL_TIM_SetCompare(htim, TIM_CHANNEL_2, brightness);
+  if (htim == &htim1) {
+    static uint32_t cnt = 0;
+    uint32_t arr_value = __HAL_TIM_GET_AUTORELOAD(htim) + 1;
+    __HAL_TIM_SetCompare(htim, TIM_CHANNEL_2, ++cnt % arr_value);
+  }
 }
